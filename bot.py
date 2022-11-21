0fce08ee394997e29d918215dacfae13abcae057
Fire <atoli.huppe@gmail.com>
2022-11-21 12:18:29 +0100

    Filter IRC messages

--- bot.py
+++ bot.py
@@ -1,24 +1,49 @@
 import asyncio
 import websockets
+from kafka import KafkaProducer
+
+
+def parsemsg(s):
+    """Breaks a message from an IRC server into its prefix, command, and arguments.
+    """
+    prefix = ''
+    trailing = []
+    if not s:
+        raise Exception("Empty line.")
+    if s[0] == ':':
+        prefix, s = s[1:].split(' ', 1)
+    if s.find(' :') != -1:
+        s, trailing = s.split(' :', 1)
+        args = s.split()
+        args.append(trailing)
+    else:
+        args = s.split()
+    command = args.pop(0)
+    return prefix, command, args
 
 
 async def handler(websocket):
+    topic = "asi322"
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
     while True:
-        message = await websocket.recv()
-        print(message)
+        rawIrcMessage = (await websocket.recv()).strip()
+        rawMessages = rawIrcMessage.split('\r\n')
+        for rawMessage in rawMessages:
+            message = parsemsg(rawMessage)
+            print(message)
+            if message[1] == 'PRIVMSG':
+                producer.send(topic, bytes(message[2][1], 'utf-8'))
 
 
 async def main() -> None:
-    topic = 
-
-    channel = "scream"
+    channel = "fire937"
     account = "asi322"
     password = "oauth:c6gx9m317pcrgro5hkyj0x4c1hwrqo"
     url = "ws://irc-ws.chat.twitch.tv:80"
-    
+
     async with websockets.connect(url) as websocket:
         await websocket.send("PASS " + password)
-        await websocket.send("NICK asi322")
+        await websocket.send("NICK " + account)
         await websocket.send("JOIN #" + channel)
         await handler(websocket)
 
--- readme.md
+++ readme.md
@@ -1,3 +1,18 @@
+Dependencies
+============
+kafka
+```
+https://kafka.apache.org/downloads
+```
+kafka-python
+```
+pip install kafka-python
+```
+websockets
+```
+pip install websockets
+```
+
 Start kafka
 ===========
 ```
@@ -9,7 +24,7 @@ bin/kafka-topics.sh --create --topic asi322 --bootstrap-server localhost:9092
 Start consumer
 ==============
 ```
-bin/kafka-console-consumer.sh --topic quickstart-events --from-beginning --bootstrap-server localhost:9092
+bin/kafka-console-consumer.sh --topic asi322 --from-beginning --bootstrap-server localhost:9092
 ```
 
 Start producer (twitch bot)
