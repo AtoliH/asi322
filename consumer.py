4ef83e8ad37595dde04f9212179b550a9b6c1985
jonoka <joris.gallene@ensta-paris.fr>
2022-11-28 10:55:49 +0000

    consumer

---
+++ consumer.py
@@ -0,0 +1,10 @@
+from kafka import KafkaConsumer
+
+topic="asi322"
+consumer = KafkaConsumer(topic,
+                         bootstrap_servers=['localhost:9092'])
+for message in consumer:
+    
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
