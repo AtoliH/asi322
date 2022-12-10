d7d655fce5a6a96e59c1e17bef09f5869e27b786
Fire <atoli.huppe@gmail.com>
2022-12-11 00:16:33 +0100

    Reformat code

--- consumer.py
+++ consumer.py
@@ -7,7 +7,7 @@ import json
 import re
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-f=open("config.json","r")
+f = open("config.json", "r")
 config = json.loads(f.read())
 f.close()
 # elastic config
@@ -19,18 +19,20 @@ es = Elasticsearch(
 topic = "asi322"
 liste_file = "badwords.txt"
 consumer = KafkaConsumer(topic, bootstrap_servers=['localhost:9092'], client_id=sys.argv[1], group_id="group_asi322")
-analyzer=SentimentIntensityAnalyzer()
+analyzer = SentimentIntensityAnalyzer()
+
 
 def send_to_elastic(message):
     resp = es.index(index="asi322", document=message)
+
 
 if __name__ == "__main__":
     account = config['twitch_bot']['account']
     password = config['twitch_bot']['token']
     headers = {
-                "Authorization": "Bearer " + password,
-                "Client-Id": config['twitch_bot']['Client_Id']
-            }
+        "Authorization": "Bearer " + password,
+        "Client-Id": config['twitch_bot']['Client_Id']
+    }
 
     with open(liste_file, "r") as badwords_file:
         print("Waiting for producer...")
@@ -40,7 +42,7 @@ if __name__ == "__main__":
             msg['user'] = msg['user'].split('!')[0]
             msg['channel'] = msg['channel']
 
-            if msg['details']['broadcaster_language']!="en":
+            if msg['details']['broadcaster_language'] != "en":
                 # Translate message to en
                 translator = Translator(to_lang="en", from_lang=msg['details']['broadcaster_language'])
                 translation = translator.translate(msg['message'])
@@ -53,7 +55,7 @@ if __name__ == "__main__":
             for word in badwords:
                 if re.compile(r'\b({0})\b'.format(word), flags=re.IGNORECASE).search(msg['translate_en']):
                     msg['badwords'].append(word)
-            
+
             vs = analyzer.polarity_scores(msg['translate_en'])
             msg['vs'] = vs
             send_to_elastic(msg)
